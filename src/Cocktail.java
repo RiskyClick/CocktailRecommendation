d0db0c09be88703c7d3022dda241e3fa621fc5c6
RiskyClick <krich@mail.sfsu.edu>
2020-02-26 15:51:24 -0800

    Implemeted a ranking a value system in hashmaps

---
+++ src/Cocktail.java
@@ -0,0 +1,34 @@
+import java.util.HashMap;
+
+public class Cocktail {
+
+    private HashMap<String, Integer> profile = new HashMap<>();
+    private int score;
+    private String name;
+
+    public Cocktail(String name) {
+        this.name = name;
+        this.score = 0;
+    }
+
+    public void add(String flavor, int rank){
+        this.profile.put(flavor, rank);
+    }
+
+    public void explore(String flavor){
+        int rank = profile.get(flavor);
+        scoreUp(rank);
+    }
+
+    public void scoreUp(int score) {
+        this.score += score;
+    }
+
+    public int getScore(){
+        return this.score;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,37 @@
 import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
+import java.nio.file.Path;
+import java.nio.file.WatchKey;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
 
+        HashMap<String, Integer> profile = new HashMap<String, Integer>();
         String[] ar = { "notsweet","sweat","bitter","notbitter","fruity","sour","refreshing"
                         ,"rum","mezcal","tequila","gin","whisky","bourbon","vodka","scotch"
                         ,"strong", "weak","floral","herbal"};
-        
+
+        Cocktail WhiskeySour = new Cocktail("Whiskey Sour");
+        Cocktail Negroni = new Cocktail("Negroni");
+        WhiskeySour.add("bitter", -3);
+        WhiskeySour.add("sweat", 3);
+        WhiskeySour.add("salty", 0);
+        WhiskeySour.add("refreshing", 3);
+        WhiskeySour.add("whisky", 5);
+        WhiskeySour.add("bourbon", 5);
+
+        Negroni.add("bitter", 4);
+        Negroni.add("sweat", -4);
+        Negroni.add("salty", 0);
+        Negroni.add("refreshing", -2);
+        Negroni.add("gin", 5);
+        Negroni.add("whisky", -5);
+
         List<String> stopWords = Arrays.asList(ar);
         ArrayList<String> finalCut = new ArrayList<>();
 
-        System.out.println("test");
+        System.out.println("What sounds good to you?");
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine().toLowerCase().replaceAll("not ", "not");
 
@@ -28,7 +44,22 @@ public class Main {
             }
         }
 
-        System.out.println(finalCut);
+        for(int i = 0; i < finalCut.size(); i++){
+            WhiskeySour.explore(finalCut.get(i));
+            Negroni.explore(finalCut.get(i));
+        }
+        System.out.println(profile.size());
+
+        if(WhiskeySour.getScore() > Negroni.getScore()){
+            System.out.println(WhiskeySour.getScore());
+            System.out.println((Negroni.getScore()));
+            System.out.println("You should try a: " + WhiskeySour.getName());
+        } else {
+            System.out.println(WhiskeySour.getScore());
+            System.out.println((Negroni.getScore()));
+            System.out.println("You should try a: " + Negroni.getName());
+        }
+
 	// write your code here
     }
 }
